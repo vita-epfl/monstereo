c1d22a0689913c4e27c2aac7e6705010957a649f
Maxime Bonnesoeur <maxime.bonn@gmail.com>
2020-10-23 15:25:22 +0200

    script to train and test the kitti dataset automatically

---
+++ eval_kitti.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+cuda_device_to_use='1'
+
+use_car='0'
+
+epochs='100'
+
+hidden_size='1024' 
+
+joints_stereo='/home/maximebonnesoeur/monstereo/data/arrays/joints-kitti-stereo-201021-1518.json'
+joints_mono='/home/maximebonnesoeur/monstereo/data/arrays/joints-kitti-201023-1410.json'
+dir_ann='/data/maxime-data/outputs_openpifpaf_kitti/annotations'
+
+joints_stereo_car='/home/maximebonnesoeur/monstereo/data/arrays/joints-kitti-vehicles-stereo-201022-1536.json'
+joints_mono_car='/home/maximebonnesoeur/monstereo/data/arrays/joints-kitti-vehicles-201022-1537.json'
+dir_ann_car='/data/maxime-data/outputs_openpifpaf_kitti/annotations_car'
+
+
+model_out () {
+    while read -r line; do
+        id=`echo $line | cut -c 1-12`    
+        if [ $id == "data/models/" ]    
+        then         
+        echo "$id"
+        model=$line  
+        echo "$model"
+        fi 
+    done <<< $1
+}
+
+
+if [ $use_car == "1" ]
+then 
+# train mono model
+output=$(CUDA_VISIBLE_DEVICES=${cuda_device_to_use} python3 -m  monstereo.run train --epochs ${epochs} --dataset kitti --joints ${joints_mono_car} --hidden_size ${hidden_size} --monocular --vehicles --dataset kitti --save)
+model_out "$output"
+model_mono="$model"
+
+echo "$model_mono"
+
+output=$(CUDA_VISIBLE_DEVICES=${cuda_device_to_use} python3 -m  monstereo.run train --epochs ${epochs} --dataset kitti --joints ${joints_stereo_car} --hidden_size ${hidden_size} --vehicles --dataset kitti --save)
+model_out "$output"
+model_stereo="$model"
+
+echo "$model_stereo"
+
+"Generate and evaluate the outputs"
+CUDA_VISIBLE_DEVICES=${cuda_device_to_use} python3 -m monstereo.run eval --dir_ann ${dir_ann_car} --model ${model_stereo}  --model-mono ${model_mono} --vehicles --generate
+
+else
+
+# train mono model
+output=$(CUDA_VISIBLE_DEVICES=${cuda_device_to_use} python3 -m  monstereo.run train --epochs ${epochs} --dataset kitti --joints ${joints_mono} --hidden_size ${hidden_size} --monocular --dataset kitti --save)
+model_out "$output"
+model_mono="$model"
+
+echo "$model_mono"
+
+output=$(CUDA_VISIBLE_DEVICES=${cuda_device_to_use} python3 -m  monstereo.run train --epochs ${epochs} --dataset kitti --joints ${joints_stereo} --hidden_size ${hidden_size} --dataset kitti --save)
+model_out "$output"
+model_stereo="$model"
+
+echo "$model_stereo"
+
+"Generate and evaluate the outputs"
+CUDA_VISIBLE_DEVICES=${cuda_device_to_use} python3 -m monstereo.run eval --dir_ann ${dir_ann} --model ${model_stereo}  --model-mono ${model_mono} --generate
+
+
+fi
