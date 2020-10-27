a898da9beac9db72e2b088253522458e6b2b429c
Maxime Bonnesoeur <maxime.bonn@gmail.com>
2020-10-27 17:56:28 +0100

    upte of the scrpts

--- eval_kitti.sh
+++ eval_kitti.sh
@@ -3,16 +3,32 @@ cuda_device_to_use='1'
 
 use_car='1'
 
-epochs='200'
+epochs='100'
 
 hidden_size='1024' 
 
 joints_stereo='/home/maximebonnesoeur/monstereo/data/arrays/joints-kitti-stereo-201021-1518.json'
-joints_mono='/home/maximebonnesoeur/monstereo/data/arrays/joints-kitti-201023-1410.json'
+joints_mono='/home/maximebonnesoeur/monstereo/data/arrays/joints-kitti-201023-1410.json' 
 dir_ann='/data/maxime-data/outputs_openpifpaf_kitti/annotations'
 
 joints_stereo_car='/home/maximebonnesoeur/monstereo/data/arrays/joints-kitti-vehicles-stereo-201022-1536.json'
+
+joints_mono_car='data/arrays/joints-kitti-vehicles-201027-1432.json' # with the new keypoints
+
+joints_mono_car='data/arrays/joints-kitti-vehicles-201027-1441.json' # with the new negative keypoints
+
+joints_mono_car='data/arrays/joints-kitti-vehicles-201027-1538.json' # with the new negative keypoints v2 -> BAD
+
 joints_mono_car='/home/maximebonnesoeur/monstereo/data/arrays/joints-kitti-vehicles-201022-1537.json'
+
+joints_mono_car='data/arrays/joints-kitti-vehicles-201027-1558.json' # with the new negative keypoints v2 -> a constant value of -5
+
+joints_mono_car='data/arrays/joints-kitti-vehicles-201027-1611.json' # with the new negative keypoints v3 -> a constant value of -1000 -> BAD
+
+joints_mono_car='data/arrays/joints-kitti-vehicles-201027-1647.json' # -> a constant value of 12000 -> BAD (-100)
+
+joints_mono_car='data/arrays/joints-kitti-vehicles-201027-1628.json' # -> a constant value of 0 -> Best results so far
+
 dir_ann_car='/data/maxime-data/outputs_openpifpaf_kitti/annotations_car'
 
 
@@ -38,34 +54,35 @@ model_mono="$model"
 
 echo "$model_mono"
 
-output=$(CUDA_VISIBLE_DEVICES=${cuda_device_to_use} python3 -m  monstereo.run train --epochs ${epochs} --dataset kitti --joints ${joints_stereo_car} --hidden_size ${hidden_size} --vehicles --dataset kitti --save)
-model_out "$output"
-model_stereo="$model"
+#output=$(CUDA_VISIBLE_DEVICES=${cuda_device_to_use} python3 -m  monstereo.run train --epochs ${epochs} --dataset kitti --joints ${joints_stereo_car} --hidden_size ${hidden_size} --vehicles --dataset kitti --save)
+#model_out "$output"
+#model_stereo="$model"
 
-echo "$model_stereo"
+#echo "$model_stereo"
 
 "Generate and evaluate the outputs"
-CUDA_VISIBLE_DEVICES=${cuda_device_to_use} python3 -m monstereo.run eval --dir_ann ${dir_ann_car} --model ${model_stereo}  --model-mono ${model_mono} --vehicles --generate
+CUDA_VISIBLE_DEVICES=${cuda_device_to_use} python3 -m monstereo.run eval --dir_ann ${dir_ann_car} --model "nope" --model_mono ${model_mono} --vehicles --generate
 
 else
 
 # train mono model
 output=$(CUDA_VISIBLE_DEVICES=${cuda_device_to_use} python3 -m  monstereo.run train --epochs ${epochs} --dataset kitti --joints ${joints_mono} --hidden_size ${hidden_size} --monocular --dataset kitti --save)
 model_out "$output"
+
+echo $output
 model_mono="$model"
 
 echo "$model_mono"
 
 output=$(CUDA_VISIBLE_DEVICES=${cuda_device_to_use} python3 -m  monstereo.run train --epochs ${epochs} --dataset kitti --joints ${joints_stereo} --hidden_size ${hidden_size} --dataset kitti --save)
 model_out "$output"
+
+echo $output
 model_stereo="$model"
 
 echo "$model_stereo"
 
 "Generate and evaluate the outputs"
-CUDA_VISIBLE_DEVICES=${cuda_device_to_use} python3 -m monstereo.run eval --dir_ann ${dir_ann} --model ${model_stereo}  --model-mono ${model_mono} --generate
-
+CUDA_VISIBLE_DEVICES=${cuda_device_to_use} python3 -m monstereo.run eval --dir_ann ${dir_ann} --model ${model_stereo}  --model_mono ${model_mono} --generate
 
 fi
-
-#CUDA_VISIBLE_DEVICES=1 python3 -m  monstereo.run predict /home/maximebonnesoeur/monstereo/data/kitti/training/image_2/00000*.png --joints_folder /home/maximebonnesoeur/monstereo/data/kitti-pifpaf/annotations_car --mode mono --model /home/maximebonnesoeur/monstereo/data/models/ms-201023-1411-vehicles.pkl --show --output_types combined --vehicles --path_gt /home/maximebonnesoeur/monstereo/data/arrays/names-kitti-vehicles-201022-1537.json --draw_box
---
+++ predict_imgs_kitti.sh
@@ -0,0 +1,23 @@
+input="/home/maximebonnesoeur/monstereo/splits/kitti_train.txt"
+input="/home/maximebonnesoeur/monstereo/splits/kitti_val.txt"
+
+
+
+output_directory='visual_tests/train_kitti'
+output_directory='visual_tests/val_kitti'
+
+dir_ann_car='data/kitti-pifpaf/annotations_car'
+
+model='data/models/ms-201027-1636-vehicles.pkl'
+path_gt='data/arrays/names-kitti-vehicles-201027-1628.json'   #Careful here, this is the name folder
+
+cuda_device_to_use='1'
+
+
+while IFS= read -r line
+do
+    CUDA_VISIBLE_DEVICES=${cuda_device_to_use} python3 -m  monstereo.run predict /home/maximebonnesoeur/monstereo/data/kitti/training/image_2/$line.png --joints_folder ${dir_ann_car} --mode mono --model ${model} --output_types combined --vehicles --path_gt ${path_gt} --draw_box --z_max 60 --output-directory ${output_directory}
+    echo "CUDA_VISIBLE_DEVICES=${cuda_device_to_use}  python3 -m  monstereo.run predict /home/maximebonnesoeur/monstereo/data/kitti/training/image_2/$line.png --joints_folder ${dir_ann_car} --mode mono --model ${model}  --show --output_types combined --vehicles --path_gt ${path_gt} --draw_box --z_max 60"
+
+  echo "$line"
+done < "$input"
